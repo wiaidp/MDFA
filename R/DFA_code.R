6172e6b8bfa6a7366f9fb036ca5375c28042a3e2
wiaidp <marcadolfrudolf.wildi@gmail.com>
2016-12-14 09:33:23 +0100

    per with plot

--- R/DFA_code.R
+++ R/DFA_code.R
@@ -27,22 +27,22 @@ per<-function(x,plot_T)
   if (abs(as.integer(len/2)-len/2)<0.1)
     DFT[k+1]<-DFT[k+1]/sqrt(2)
   per<-abs(DFT)^2
-#  if (plot_T)
-#  {
-#    par(mfrow=c(2,1))
-#    plot(per,type="l",axes=F,xlab="Frequency",ylab="Periodogram",
-#         main="Periodogram")
-#    axis(1,at=1+0:6*len/12,labels=c("0","pi/6","2pi/6","3pi/6",
-#                                    "4pi/6","5pi/6","pi"))
-#    axis(2)
-#    box()
-#    plot(log(per),type="l",axes=F,xlab="Frequency",ylab="Log-periodogram",
-#         main="Log-periodogram")
-#    axis(1,at=1+0:6*len/12,labels=c("0","pi/6","2pi/6","3pi/6",
-#                                    "4pi/6","5pi/6","pi"))
-#    axis(2)
-#    box()
-#  }
+  if (plot_T)
+  {
+    par(mfrow=c(2,1))
+    plot(per,type="l",axes=F,xlab="Frequency",ylab="Periodogram",
+         main="Periodogram")
+    axis(1,at=1+0:6*len/12,labels=c("0","pi/6","2pi/6","3pi/6",
+                                    "4pi/6","5pi/6","pi"))
+    axis(2)
+    box()
+    plot(log(per),type="l",axes=F,xlab="Frequency",ylab="Log-periodogram",
+         main="Log-periodogram")
+    axis(1,at=1+0:6*len/12,labels=c("0","pi/6","2pi/6","3pi/6",
+                                    "4pi/6","5pi/6","pi"))
+    axis(2)
+    box()
+  }
   return(list(DFT=DFT,per=per))
 }
 
